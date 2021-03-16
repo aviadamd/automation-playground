89cdcfdf3416249d662259a3ea29516870c54470
aviadamd <aviadamd@gmail.com>
2021-03-16 23:09:35 +0200

    update

--- src/main/java/utilities/Listeners.java
+++ src/main/java/utilities/Listeners.java
@@ -1,6 +1,7 @@
 package utilities;
 
 import io.qameta.allure.Attachment;
+import io.qameta.allure.Description;
 import lombok.extern.slf4j.Slf4j;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
--- src/test/java/grafanas/GrafanaLoginTest.java
+++ src/test/java/grafanas/GrafanaLoginTest.java
@@ -11,7 +11,6 @@ public class GrafanaLoginTest extends SharedMethods {
     @Test(description = "test 01 : login to grafana")
     @Description("login to grafana with valid user name and pass word")
     public void test01_login() {
-
         //Allure.addAttachment("demo print","air please");
         GrafanaShared grafanaShared = new GrafanaShared();
         grafanaShared.login("admin", "5311072BsDvir");
