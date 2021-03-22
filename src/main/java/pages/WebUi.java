79e625351132fee42367885a6f0895ac16e9f2ec
aviadamd <aviadamd@gmail.com>
2021-03-22 07:32:26 +0200

    update

--- src/main/java/pages/WebUi.java
+++ src/main/java/pages/WebUi.java
@@ -2,15 +2,11 @@ package pages;
 
 import org.openqa.selenium.WebDriver;
 import pages.accountManagement.AccountManagementLoginPage;
-import pages.grafana.GrafanaLoginPage;
 
 public class WebUi {
-
-    public GrafanaLoginPage grafanaLoginPage;
     public AccountManagementLoginPage accountManagementLoginPage;
 
     public WebUi(WebDriver driver) {
-        this.grafanaLoginPage = new GrafanaLoginPage(driver);
         this.accountManagementLoginPage = new AccountManagementLoginPage(driver);
     }
 }
