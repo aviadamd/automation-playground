7900f32653b428aa2ad1c1e5e1b5d213ae144c11
aviadamd <aviadamd@gmail.com>
2021-03-22 21:14:43 +0200

    update

--- src/test/java/test/accountManagement/PrintTest.java
+++ src/test/java/test/accountManagement/PrintTest.java
@@ -9,8 +9,8 @@ import org.testng.annotations.Test;
 @org.testng.annotations.Listeners({Listeners.class})
 public class PrintTest {
 
-    @Test(description = "test 01 : login to account management")
-    @Description("login to account management with valid user name and pass word")
+    @Test(description = "test 01 : print test")
+    @Description("print test")
     public void printMethod() {
         log.debug("print form here");
     }
