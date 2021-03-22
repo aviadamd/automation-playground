8e9d1c69c460e05c9cda9391966241337b34ab4d
aviadamd <aviadamd@gmail.com>
2021-03-22 21:12:06 +0200

    update

--- src/test/java/test/accountManagement/PrintTest.java
+++ src/test/java/test/accountManagement/PrintTest.java
@@ -4,11 +4,10 @@ import base.baseUtilities.Listeners;
 import io.qameta.allure.Description;
 import lombok.extern.slf4j.Slf4j;
 import org.testng.annotations.Test;
-import test.TestMethodsObjects;
 
 @Slf4j
 @org.testng.annotations.Listeners({Listeners.class})
-public class AccountManagementTest extends TestMethodsObjects {
+public class PrintTest {
 
     @Test(description = "test 01 : login to account management")
     @Description("login to account management with valid user name and pass word")
