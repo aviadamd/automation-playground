3ecb38f1946c27846a656af5a97a5ed1d3fdf079
aviadamd <aviadamd@gmail.com>
2021-03-22 08:13:44 +0200

    update

--- src/test/java/test/accountManagement/AccountManagementTest.java
+++ src/test/java/test/accountManagement/AccountManagementTest.java
@@ -2,15 +2,17 @@ package test.accountManagement;
 
 import base.baseUtilities.Listeners;
 import io.qameta.allure.Description;
+import lombok.extern.slf4j.Slf4j;
 import org.testng.annotations.Test;
 import test.TestMethodsObjects;
 
+@Slf4j
 @org.testng.annotations.Listeners({Listeners.class})
 public class AccountManagementTest extends TestMethodsObjects {
 
     @Test(description = "test 01 : login to account management")
     @Description("login to account management with valid user name and pass word")
     public void test01_login() {
-        accountManagementShared().login("!wiz1017", "!bl123456");
+        log.debug("print form " + AccountManagementTest.class.getName());
     }
 }
