5aff8b0cabe699059dc26c3377c3f53a09a67172
aviadamd <aviadamd@gmail.com>
2021-03-24 23:02:48 +0200

    update

--- src/main/java/base/baseUtilities/Base.java
+++ src/main/java/base/baseUtilities/Base.java
@@ -13,8 +13,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import ru.yandex.qatools.ashot.Screenshot;
 import ru.yandex.qatools.ashot.comparison.ImageDiff;
 import ru.yandex.qatools.ashot.comparison.ImageDiffer;
@@ -25,6 +23,7 @@ import java.util.Optional;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
+@Slf4j
 public class Base {
 
     public static WebDriver driver;
@@ -33,7 +32,6 @@ public class Base {
     public static Screenshot imageScreenShot;
     public static ImageDiff imageDiff;
     public ImageDiffer imageDiffer = new ImageDiffer();
-    public static final Logger log = LoggerFactory.getLogger(Base.class);
 
     @SneakyThrows
     public Base() {
