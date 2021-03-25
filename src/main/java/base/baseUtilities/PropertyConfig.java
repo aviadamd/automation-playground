2fb83352a3ad76a5ddffdc0f99e66552a1206fa9
aviadamd <aviadamd@gmail.com>
2021-03-25 16:43:19 +0200

    update

--- src/main/java/base/baseUtilities/PropertyConfig.java
+++ src/main/java/base/baseUtilities/PropertyConfig.java
@@ -1,13 +1,11 @@
 package base.baseUtilities;
 
 import lombok.SneakyThrows;
-
 import java.io.FileInputStream;
 import java.util.Properties;
 
 public class PropertyConfig {
 
-    private static Properties prop;
     public String platform;
     public String platformType;
     public String url;
@@ -15,12 +13,15 @@ public class PropertyConfig {
     public String androidSdk;
     public String nodeJs;
     public String appPath;
+    private static Properties properties;
 
     @SneakyThrows
     public PropertyConfig(String path) {
-        prop = new Properties();
-        FileInputStream ip = new FileInputStream(System.getProperty("user.dir") + path);
-        prop.load(ip);
+        properties = new Properties();
+        FileInputStream fileInputStream =
+                new FileInputStream(System.getProperty("user.dir") + path);
+        properties.load(fileInputStream);
+
         platform = setProperties("platform");
         platformType = setProperties("platformType");
         url = setProperties("url");
@@ -28,13 +29,14 @@ public class PropertyConfig {
         androidSdk = setProperties("androidSdk");
         nodeJs = setProperties("nodeJs");
         appPath = setProperties("appPath");
-        ip.close();
+
+        fileInputStream.close();
     }
 
     private static String setProperties(String proName) {
         if (System.getProperty(proName) != null)
              return System.getProperty(proName);
-        else return prop.getProperty(proName);
+        else return properties.getProperty(proName);
     }
 
 }
