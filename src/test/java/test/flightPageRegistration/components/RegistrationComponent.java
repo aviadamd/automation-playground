ea4dad7bf673f197e6381e62ab295664725ca860
aviadamd <aviadamd@gmail.com>
2021-03-28 00:58:40 +0300

    update

--- src/test/java/test/flightPageRegistration/components/RegistrationComponent.java
+++ src/test/java/test/flightPageRegistration/components/RegistrationComponent.java
@@ -1,7 +1,6 @@
 package test.flightPageRegistration.components;
 
 import lombok.extern.slf4j.Slf4j;
-import org.openqa.selenium.WebElement;
 import test.BaseTest;
 
 @Slf4j
@@ -11,20 +10,15 @@ public class RegistrationComponent extends BaseTest {
         utilities.uiActions().elementPresented(flightUi.registrationPage.firstNameTxt,5);
     }
 
-    private void sendKeys(WebElement element, String key) {
-        element.sendKeys(key);
-        System.out.println("send " + key + " to " + element.toString());
-    }
-
     public void enterUserDetails(String firstName, String lastName){
-        sendKeys(flightUi.registrationPage.firstNameTxt, firstName);
-        sendKeys(flightUi.registrationPage.lastNameTxt, lastName);
+        utilities.uiActions().sendKeys(flightUi.registrationPage.firstNameTxt, firstName);
+        utilities.uiActions().sendKeys(flightUi.registrationPage.lastNameTxt, lastName);
     }
 
     public void enterUserCredentials(String username, String password){
-        sendKeys(flightUi.registrationPage.usernameTxt, username);
-        sendKeys(flightUi.registrationPage.passwordTxt, password);
-        sendKeys(flightUi.registrationPage.confirmPasswordTxt, password);
+        utilities.uiActions().sendKeys(flightUi.registrationPage.usernameTxt, username);
+        utilities.uiActions().sendKeys(flightUi.registrationPage.passwordTxt, password);
+        utilities.uiActions().sendKeys(flightUi.registrationPage.confirmPasswordTxt, password);
     }
 
     public void submit(){
